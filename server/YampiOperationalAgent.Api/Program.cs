f0511868f460f571d2778174f9145f13c42bb74e
Alef <alefnatan.g32@outlook.com>
2026-07-30 11:40:34 -0300

    feat(persistence): register PostgreSQL persistence in API

--- server/YampiOperationalAgent.Api/Program.cs
+++ server/YampiOperationalAgent.Api/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
